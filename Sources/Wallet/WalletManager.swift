9635133e82c15e3398c5187cff0e818314318b58
ceyx <lh_halcyondays@163.com>
2019-02-22 14:04:25 +0800

    remove redundant param

--- Sources/Wallet/WalletManager.swift
+++ Sources/Wallet/WalletManager.swift
@@ -10,197 +10,210 @@ import Foundation
 import CoreBitcoin
 
 public struct WalletManager {
-  public static func importFromMnemonic(_ mnemonic: String, metadata: WalletMeta, encryptBy password: String, at path: String) throws -> BasicWallet {
-    let identity = try IdentityValidator().validate()
-    return try identity.importFromMnemonic(mnemonic, metadata: metadata, encryptBy: password, at: path)
-  }
-
-  /**
-   Import ETH keystore json to generate wallet
-
-   - parameter keystore: JSON text
-   - parameter password: Password of keystore
-   - parameter metadata: Wallet metadata
-   */
-  public static func importFromKeystore(_ keystore: JSONObject, encryptedBy password: String, metadata: WalletMeta) throws -> BasicWallet {
-    let identity = try IdentityValidator().validate()
-    _ = try V3KeystoreValidator(keystore).validate()
-    return try identity.importFromKeystore(keystore, encryptedBy: password, metadata: metadata)
-  }
-
-
-  public static func findWalletByPrivateKey(_ privateKey: String, on chainType: ChainType, network: Network? = nil, segWit: SegWit = .none) throws -> BasicWallet? {
-    let identity = try IdentityValidator().validate()
-    return try identity.findWalletByPrivateKey(privateKey, on: chainType, network: network, segWit: segWit)
-  }
-
-  public static func findWalletByMnemonic(_ mnemonic: String, on chainType: ChainType, path: String, network: Network? = nil, segWit: SegWit = .none) throws -> BasicWallet? {
-    let identity = try IdentityValidator().validate()
-    return try identity.findWalletByMnemonic(mnemonic, on: chainType, path: path, network: network, segWit: segWit)
-  }
-
-  public static func findWalletByKeystore(_ keystore: [String: Any], on chainType: ChainType, password: String) throws -> BasicWallet? {
-    let identity = try IdentityValidator().validate()
-    return try identity.findWalletByKeystore(keystore, on: chainType, password: password)
-  }
-
-  public static func findWalletByWalletID(_ walletID: String) throws -> BasicWallet {
-    let identity = try IdentityValidator().validate()
-    guard let wallet = identity.findWalletByWalletID(walletID) else {
-      throw GenericError.walletNotFound
+    public static func importFromMnemonic(_ mnemonic: String, metadata: WalletMeta, encryptBy password: String, at path: String) throws -> BasicWallet {
+        let identity = try IdentityValidator().validate()
+        return try identity.importFromMnemonic(mnemonic, metadata: metadata, encryptBy: password, at: path)
     }
 
-    return wallet
-  }
+    /**
+     Import ETH keystore json to generate wallet
 
-  public static func findWalletByAddress(_ address: String, on chainType: ChainType) throws -> BasicWallet {
-    let identity = try IdentityValidator().validate()
-    guard let wallet = identity.findWalletByAddress(address, on: chainType) else {
-      throw GenericError.walletNotFound
+     - parameter keystore: JSON text
+     - parameter password: Password of keystore
+     - parameter metadata: Wallet metadata
+     */
+    public static func importFromKeystore(_ keystore: JSONObject, encryptedBy password: String, metadata: WalletMeta) throws -> BasicWallet {
+        let identity = try IdentityValidator().validate()
+        _ = try V3KeystoreValidator(keystore).validate()
+        return try identity.importFromKeystore(keystore, encryptedBy: password, metadata: metadata)
     }
 
-    return wallet
-  }
 
-
-
-  /**
-   Sign transaction with given parameters
-
-   - returns signed data
-   */
-  public static func ethSignTransaction(
-    wallet:BasicWallet,
-    nonce: String,
-    gasPrice: String,
-    gasLimit: String,
-    to: String,
-    value: String,
-    data: String,
-    password: String,
-    chainID: Int
-  ) throws -> TransactionSignedResult {
-
-    let privateKey = try wallet.privateKey(password: password)
-
-    let raw: [String: String] = [
-      "nonce": nonce,
-      "gasPrice": gasPrice,
-      "gasLimit": gasLimit,
-      "to": to,
-      "value": value,
-      "data": data
-    ]
-    let tx = ETHTransaction(raw: raw, chainID: chainID)
-    _ = tx.sign(with: privateKey)
-
-    return tx.signedResult
-  }
-
-  public static func btcSignTransaction(
-    wallet: BasicWallet,
-    to: String,
-    amount: Int64,
-    fee: Int64,
-    password: String,
-    outputs: [[String: Any]],
-    changeIdx: Int,
-    isTestnet: Bool,
-    segWit: SegWit,
-    isExternal:Bool = false
-  ) throws -> TransactionSignedResult {
-
-    guard let toAddress = BTCAddress(string: to) else {
-      throw AddressError.invalid
+    public static func findWalletByPrivateKey(_ privateKey: String, on chainType: ChainType, network: Network? = nil, segWit: SegWit = .none) throws -> BasicWallet? {
+        let identity = try IdentityValidator().validate()
+        return try identity.findWalletByPrivateKey(privateKey, on: chainType, network: network, segWit: segWit)
     }
 
-    let utxos: [UTXO] = try outputs.map { output in
-      guard let utxo = UTXO(raw: output) else {
-        throw GenericError.paramError
-      }
-      return utxo
+    public static func findWalletByMnemonic(_ mnemonic: String, on chainType: ChainType, path: String, network: Network? = nil, segWit: SegWit = .none) throws -> BasicWallet? {
+        let identity = try IdentityValidator().validate()
+        return try identity.findWalletByMnemonic(mnemonic, on: chainType, path: path, network: network, segWit: segWit)
     }
 
-    let changeKey: BTCKey
-    let privateKeys: [BTCKey]
-
-    if wallet.metadata.walletFrom == .wif {
-      let wif = try wallet.privateKey(password: password)
-      changeKey = BTCKey(wif: wif)
-      privateKeys = Array(repeating: changeKey, count: outputs.count)
-    } else {
-      let extendedKey = try wallet.privateKey(password: password)
-      guard let keychain = BTCKeychain(extendedKey: extendedKey),
-            let key = isExternal ? keychain.externalKey(at: UInt32(changeIdx)) :keychain.changeKey(at: UInt32(changeIdx)) else {
-          throw GenericError.unknownError
-      }
-      changeKey = key
-      privateKeys = utxos.map { output in
-        let pathWithSlash = "/\(output.derivedPath ?? "")"
-        let key = keychain.key(withPath: pathWithSlash)!
-        key.isPublicKeyCompressed = true
-        return key
-      }
+    public static func findWalletByKeystore(_ keystore: [String: Any], on chainType: ChainType, password: String) throws -> BasicWallet? {
+        let identity = try IdentityValidator().validate()
+        return try identity.findWalletByKeystore(keystore, on: chainType, password: password)
     }
 
-    let changeAddress = changeKey.address(on: isTestnet ? .testnet : .mainnet, segWit: segWit)
-    let signer = try BTCTransactionSigner(utxos: utxos, keys: privateKeys, amount: amount, fee: fee, toAddress: toAddress, changeAddress: changeAddress)
+    public static func findWalletByWalletID(_ walletID: String) throws -> BasicWallet {
+        let identity = try IdentityValidator().validate()
+        guard let wallet = identity.findWalletByWalletID(walletID) else {
+            throw GenericError.walletNotFound
+        }
 
-    if segWit.isSegWit {
-      return try signer.signSegWit()
-    } else {
-      return try signer.sign()
-    }
-  }
-
-  /// Allow BTC wallet to switch between legacy/SegWit.
-  public static func switchBTCWalletMode(walletID: String, password: String, segWit: SegWit) throws -> BasicWallet {
-     let wallet = try findWalletByWalletID(walletID)
-
-    if wallet.metadata.chain != .btc {
-      throw GenericError.operationUnsupported
+        return wallet
     }
 
-    if wallet.metadata.segWit == segWit {
-      return wallet
+    public static func findWalletByAddress(_ address: String, on chainType: ChainType) throws -> BasicWallet {
+        let identity = try IdentityValidator().validate()
+        guard let wallet = identity.findWalletByAddress(address, on: chainType) else {
+            throw GenericError.walletNotFound
+        }
+
+        return wallet
     }
 
-    let newKeystore: Keystore
-    var metadata = wallet.metadata
-    metadata.segWit = segWit
-    let path = BIP44.path(for: metadata.network, segWit: segWit)
 
-    if let mnemonicKeystore = wallet.keystore as? EncMnemonicKeystore {
-      guard wallet.keystore.verify(password: password) else {
-        throw PasswordError.incorrect
-      }
-      let mnemonic = mnemonicKeystore.decryptMnemonic(password)
+    /**
+     Sign transaction with given parameters
 
-      newKeystore = try BTCMnemonicKeystore(
-        password: password,
-        mnemonic: mnemonic,
-        path: path,
-        metadata: metadata,
-        id: walletID
-      )
-    } else {
-      // private key export already verifies password
-      let privateKey = try wallet.privateKey(password: password)
-      newKeystore = try BTCKeystore(
-        password: password,
-        wif: privateKey,
-        metadata: metadata,
-        id: walletID
-      )
+     - returns signed data
+     */
+    public static func ethSignTransaction(
+            wallet: BasicWallet,
+            nonce: String,
+            gasPrice: String,
+            gasLimit: String,
+            to: String,
+            value: String,
+            data: String,
+            password: String,
+            chainID: Int
+    ) throws -> TransactionSignedResult {
+
+        let privateKey = try wallet.privateKey(password: password)
+
+        let raw: [String: String] = [
+            "nonce": nonce,
+            "gasPrice": gasPrice,
+            "gasLimit": gasLimit,
+            "to": to,
+            "value": value,
+            "data": data
+        ]
+        let tx = ETHTransaction(raw: raw, chainID: chainID)
+        _ = tx.sign(with: privateKey)
+
+        return tx.signedResult
     }
-    // check if the new walle will override the wallet derived by identity
+
+    public static func btcSignTransaction(
+            wallet: BasicWallet,
+            to: String,
+            amount: Int64,
+            fee: Int64,
+            password: String,
+            outputs: [[String: Any]],
+            changeIdx: Int,
+            isExternal: Bool = false
+    ) throws -> TransactionSignedResult {
+
+        let isTestnet = !wallet.metadata.isMainnet
+        let segWit = wallet.metadata.segWit
+
+        guard let toAddress = BTCAddress(string: to) else {
+            throw AddressError.invalid
+        }
+
+        let utxos: [UTXO] = try outputs.map { output in
+            guard let utxo = UTXO(raw: output) else {
+                throw GenericError.paramError
+            }
+            return utxo
+        }
+
+        let changeKey: BTCKey
+        let privateKeys: [BTCKey]
+
+        if wallet.metadata.walletFrom == .wif {
+            let wif = try wallet.privateKey(password: password)
+            changeKey = BTCKey(wif: wif)
+            privateKeys = Array(repeating: changeKey, count: outputs.count)
+        } else {
+            let extendedKey = try wallet.privateKey(password: password)
+            guard let keychain = BTCKeychain(extendedKey: extendedKey),
+                  let key = isExternal ? keychain.externalKey(at: UInt32(changeIdx)) : keychain.changeKey(at: UInt32(changeIdx)) else {
+                throw GenericError.unknownError
+            }
+            changeKey = key
+            privateKeys = utxos.map { output in
+                let pathWithSlash = "/\(output.derivedPath ?? "")"
+                let key = keychain.key(withPath: pathWithSlash)!
+                key.isPublicKeyCompressed = true
+                return key
+            }
+        }
+
+        let changeAddress = changeKey.address(on: isTestnet ? .testnet : .mainnet, segWit: segWit)
+        let signer = try BTCTransactionSigner(utxos: utxos, keys: privateKeys, amount: amount, fee: fee, toAddress: toAddress, changeAddress: changeAddress)
+
+        if segWit.isSegWit {
+            return try signer.signSegWit()
+        } else {
+            return try signer.sign()
+        }
+    }
+
+    public static func usdtSignTransaction(
+            wallet: BasicWallet,
+            to: String,
+            amount: Int64,
+            fee: Int64,
+            password: String,
+            outputs: [[String: Any]],
+            changeIdx: Int,
+            isExternal: Bool = false
+    ) throws -> TransactionSignedResult {
+
+    }
+
+    /// Allow BTC wallet to switch between legacy/SegWit.
+    public static func switchBTCWalletMode(walletID: String, password: String, segWit: SegWit) throws -> BasicWallet {
+        let wallet = try findWalletByWalletID(walletID)
+
+        if wallet.metadata.chain != .btc {
+            throw GenericError.operationUnsupported
+        }
+
+        if wallet.metadata.segWit == segWit {
+            return wallet
+        }
+
+        let newKeystore: Keystore
+        var metadata = wallet.metadata
+        metadata.segWit = segWit
+        let path = BIP44.path(for: metadata.network, segWit: segWit)
+
+        if let mnemonicKeystore = wallet.keystore as? EncMnemonicKeystore {
+            guard wallet.keystore.verify(password: password) else {
+                throw PasswordError.incorrect
+            }
+            let mnemonic = mnemonicKeystore.decryptMnemonic(password)
+
+            newKeystore = try BTCMnemonicKeystore(
+                    password: password,
+                    mnemonic: mnemonic,
+                    path: path,
+                    metadata: metadata,
+                    id: walletID
+            )
+        } else {
+            // private key export already verifies password
+            let privateKey = try wallet.privateKey(password: password)
+            newKeystore = try BTCKeystore(
+                    password: password,
+                    wif: privateKey,
+                    metadata: metadata,
+                    id: walletID
+            )
+        }
+        // check if the new walle will override the wallet derived by identity
 //    if Identity.currentIdentity?.findWalletByAddress(newKeystore.address, on: .btc) != nil {
 //      throw AddressError.alreadyExist
 //    }
 
-    wallet.keystore = newKeystore
-    return wallet
-  }
-  
+        wallet.keystore = newKeystore
+        return wallet
+    }
+
 
 }
--- Tests/Transaction/BitcoinTransactionTests.swift
+++ Tests/Transaction/BitcoinTransactionTests.swift
@@ -36,15 +36,14 @@ class BitcoinTransactionTests: TestCase {
             ]
             let transaction = try WalletManager.btcSignTransaction(
                     wallet: wallet, to: to, amount: amount,
-                    fee: fee, password: SampleKey.password, outputs: outputs, changeIdx: 0,
-                    isTestnet: true, segWit: SegWit.none,isExternal: true)
+                    fee: fee, password: SampleKey.password, outputs: outputs, changeIdx: 0, isExternal: true)
             let signedTx: String = transaction.signedTx
             let txHash: String = transaction.txHash
             print("signedTx:\(signedTx)")
-            print("txHash:\(txHash)")
-            XCTAssertEqual(txHash, "3315032025bb05c1ce2ab5a8b55c5af2112084872199d69722e926feb11ec94f")
+            print("txHash:\(txHash)" )
+            XCTAssertEqual(txHash, "812a91ad0a53ac635f630e864cfe91cd60bd92739dddc8f7f93b86702ed64580")
             XCTAssertEqual(signedTx,
-                    "01000000024c8260623e282604c8d69a20ca7522c0a8487798eadbf2b7e86d8d6f0ab40123000000006b483045022100a7ba235b47258ebff6a45b2a50c18ea466645669ed1d4a1460abae4bf044138902206c945ed52fa865e9da52129b8e8632ca18389e22c1598b030c3568f32acf70a8012102ce52520652ca5b04aed0808a5d301c749f21a304885d11663f89859f9509f6eeffffffff6acebe60d6705511290311eb154c6d95a445a685ee3c32e5eba5cfe23722087d000000006a473044022016e45757d63bcf72fdb4760b98d47851ae977997eb629c2df823311aa91c8a1e0220125351ef2db933b7ea46aac886c8fc4c442ca092d49ef9872e6d4f03b5917064012102ce52520652ca5b04aed0808a5d301c749f21a304885d11663f89859f9509f6eeffffffff0128230000000000001976a914fb293a287ddcb136d5d2c86cd203feba5b58689e88ac00000000")
+                    "01000000023eb57bd4dfc8d58ce955d91ef0066d3a524183599f0fb2ac08313e88bba338b4000000006a473044022018a196c2a714262259ff923d6f0ee6a2d0381b356fa45ce46ef4b0bfcae68b46022003cc5b1f7290aa7e5bc8781e814d67bfbbc679bd3554c34d4dc14d5dee9956c9012102ce52520652ca5b04aed0808a5d301c749f21a304885d11663f89859f9509f6eeffffffff07f506f6ab8a065ad185dcb09ad222d95527523dc2596cee3428937ffc292840000000006a47304402201fe0c63693303917a65262b921525fcca9387914148ec22a4ad992a0908c475502202ce46618bd4a361b2af7f1da973188e4b7fbe5cebb863e258bfd4c709e7c6f76012102ce52520652ca5b04aed0808a5d301c749f21a304885d11663f89859f9509f6eeffffffff0228230000000000001976a914fb293a287ddcb136d5d2c86cd203feba5b58689e88ac4a143c02000000001976a9147f9ee0c3ecb167027680c975ba2d4e161430afa688ac00000000")
         } catch {
             XCTFail("error:\(error)")
         }
